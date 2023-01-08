4a8c7cc59e181a7964a7267b2f80d83ef33d5f95
joshcbrown <80245312+joshcbrown@users.noreply.github.com>
2023-01-08 12:44:56 +1100

    add rough pomodoro functionality

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Pogodoro -- A poggers-as-hell terminal UI pomodoro timer
+# pogodoro — A poggers-as-hell terminal UI pomodoro timer
 
-> This is a small project that I'm doing in order to learn Rust. Expect jank and bugs. Big shoutout to [orhun](https://github.com/orhun/)'s [tui-rs template](https://github.com/orhun/rust-tui-template), which I am using as a base for this project.
+This is a small project that I'm doing in order to learn Rust. Expect jank and bugs. Big shoutout to [orhun](https://github.com/orhun/)'s [tui-rs template](https://github.com/orhun/rust-tui-template), which I am using as a base for this project.
 
--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 use std::error;
+use std::time::{Duration, Instant};
 use tui::backend::Backend;
 use tui::layout::Alignment;
 use tui::style::{Color, Style};
@@ -13,11 +14,19 @@ pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 pub struct App {
     /// Is the application running?
     pub running: bool,
+    pomo: Pomodoro,
 }
 
 impl Default for App {
     fn default() -> Self {
-        Self { running: true }
+        Self {
+            running: true,
+            pomo: Pomodoro::new(
+                Duration::from_secs(2),
+                Duration::from_secs(5),
+                Duration::from_secs(10),
+            ),
+        }
     }
 }
 
@@ -28,7 +37,9 @@ impl App {
     }
 
     /// Handles the tick event of the terminal.
-    pub fn tick(&self) {}
+    pub fn tick(&mut self) {
+        self.pomo.update()
+    }
 
     /// Renders the user interface widgets.
     pub fn render<B: Backend>(&mut self, frame: &mut Frame<'_, B>) {
@@ -38,7 +49,12 @@ impl App {
         // - https://github.com/fdehau/tui-rs/tree/master/examples
         frame.render_widget(
             Paragraph::new(
-                "This is a tui-rs template.\nPress `Esc`, `Ctrl-C` or `q` to stop running.",
+                format!(
+                "This is a tui-rs template.\nPress `Esc`, `Ctrl-C` or `q` to stop running.\nTimer: {}\nState: {:?}\nFinished: {}",
+                self.pomo.current.to_string(),
+                self.pomo.state,
+                self.pomo.pomos_completed
+                )
             )
             .block(
                 Block::default()
@@ -51,5 +67,86 @@ impl App {
             .alignment(Alignment::Center),
             frame.size(),
         )
+    }
+}
+
+#[derive(Debug)]
+struct Timer {
+    start_time: Instant,
+    dur: Duration,
+    elapsed: Duration,
+}
+
+impl Timer {
+    fn new(dur: Duration) -> Self {
+        Self {
+            dur,
+            start_time: Instant::now(),
+            elapsed: Duration::from_secs(0),
+        }
+    }
+
+    fn update(&mut self) {
+        // might check here later if timer is over
+        self.elapsed = self.start_time.elapsed();
+    }
+
+    fn is_finished(&self) -> bool {
+        // println!("{:#?} {:#?}", self.total_time, self.elapsed);
+        self.elapsed >= self.dur
+    }
+    fn to_string(&self) -> String {
+        if self.is_finished() {
+            "finished!".into()
+        } else {
+            let to_go = (self.dur - self.elapsed + Duration::from_secs(1)).as_secs();
+            format!("{}m{}s", to_go / 60, to_go % 60)
+        }
+    }
+}
+
+#[derive(Debug)]
+enum PomodoroState {
+    Work,
+    Break,
+}
+
+#[derive(Debug)]
+struct Pomodoro {
+    current: Timer,
+    work_dur: Duration,
+    short_break_dur: Duration,
+    long_break_dur: Duration,
+    state: PomodoroState,
+    pomos_completed: u16,
+}
+
+impl Pomodoro {
+    fn new(work_dur: Duration, short_break_dur: Duration, long_break_dur: Duration) -> Self {
+        Self {
+            current: Timer::new(work_dur),
+            state: PomodoroState::Work,
+            pomos_completed: 0,
+            work_dur,
+            short_break_dur,
+            long_break_dur,
+        }
+    }
+
+    fn update(&mut self) {
+        self.current.update();
+        if self.current.is_finished() {
+            (self.state, self.current) = match self.state {
+                PomodoroState::Work => {
+                    self.pomos_completed += 1;
+                    if self.pomos_completed % 4 == 0 {
+                        (PomodoroState::Break, Timer::new(self.long_break_dur))
+                    } else {
+                        (PomodoroState::Break, Timer::new(self.short_break_dur))
+                    }
+                }
+                PomodoroState::Break => (PomodoroState::Work, Timer::new(self.work_dur)),
+            }
+        }
     }
 }
